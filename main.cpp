2073f2b966eb7482abddb1cd43990deaca05406e
Emily Tseng <yiwentseng.a@gmail.com>
2024-04-12 14:36:04 -0400

    finish checking

--- main.cpp
+++ main.cpp
@@ -30,14 +30,13 @@ void generate_combinations(vector<vector<char>> &combinations, int snp_size)
 // build the bit table for the dataset
 void build_bit_table(vector<vector<char>> &data, vector<vector<vector<bitset<64>>>> &bit_table, int size, int snp_size)
 {
-    // snp_size * 3 * number of samples
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < snp_size; i++)
     {
-        for (int j = 0; j < snp_size; j++)
+        for (int j = 0; j < size; j++)
         {
             int x = j / 64;
             int y = j % 64;
-            bit_table[j][data[i][j] - '0'][x][y] = 1;
+            bit_table[i][data[j][i] - '0'][x][y] = 1;
         }
     }
 }
@@ -110,16 +109,22 @@ int main(int argc, char *argv[])
     int control_size = 0;
     int case_size = 0;
     int snp_size = 0;
+    bool debug = true; // TODO: set to false if do not want to print out debug info
 
     // read the dataset
     fstream fin;
     fin.open(argv[1], ios::in);
     string line, word;
     vector<char> sample;
+
+    // (number of control samples) * snp_size
     vector<vector<char>> control_data;
+
+    // (number of case samples ) * snp_size
     vector<vector<char>> case_data;
     while (getline(fin, line, '\n'))
     {
+        // cout << "here" << endl;
         istringstream s(line);
         while (std::getline(s, word, ','))
         {
@@ -127,11 +132,12 @@ int main(int argc, char *argv[])
             {
                 break;
             }
+            // cout << word[0] << endl;
             sample.push_back(word[0]);
         }
         if (sample.size() > 0)
         {
-            if (sample[sample.size() - 1] == 0)
+            if (sample[sample.size() - 1] == '0')
             {
                 sample.pop_back();
                 control_data.push_back(sample);
@@ -148,15 +154,61 @@ int main(int argc, char *argv[])
         }
     }
     fin.close();
-
     // get the number of snps
-    int snp_size = control_data[0].size();
+    snp_size = control_data[0].size();
 
-    // generate 3 order combinations
+    if (debug)
+    {
+        cout << "first stage: read in data\n"
+             << endl;
+
+        cout << "this is control data: " << endl;
+        for (int i = 0; i < control_size; i++)
+        {
+            cout << "first sample: ";
+            for (int j = 0; j < snp_size; j++)
+            {
+                cout << control_data[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+
+        cout << "this is case data: " << endl;
+        for (int i = 0; i < case_size; i++)
+        {
+            cout << "first sample: ";
+            for (int j = 0; j < snp_size; j++)
+            {
+                cout << case_data[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << "-----------------------------------------------" << endl;
+        cout << endl;
+    }
+
+    // generate 3 order combinations (each row is a combination)
     vector<vector<char>> combinations;
     generate_combinations(combinations, snp_size);
 
+    if (debug)
+    {
+        cout << "second stage: generate the number of combinations of snps\n"
+             << endl;
+        for (auto &combination : combinations)
+        {
+            cout << combination[0] << " " << combination[1] << " " << combination[2] << endl;
+        }
+        cout << "\nthere are " << combinations.size() << " of combinations in total." << endl;
+
+        cout << "-----------------------------------------------------" << endl;
+    }
+
     // initialize the bit table
+    // dimension: snp_size * 3 * (number of 64 multiple in the sample (ceiling))
     vector<vector<vector<bitset<64>>>> control_bit_table(snp_size, vector<vector<bitset<64>>>(3, vector<bitset<64>>(ceil(control_size * 1.0 / 64), 0)));
     vector<vector<vector<bitset<64>>>> case_bit_table(snp_size, vector<vector<bitset<64>>>(3, vector<bitset<64>>(ceil(case_size * 1.0 / 64), 0)));
 
@@ -164,7 +216,47 @@ int main(int argc, char *argv[])
     build_bit_table(control_data, control_bit_table, control_size, snp_size);
     build_bit_table(case_data, case_bit_table, case_size, snp_size);
 
+    if (debug)
+    {
+        cout << "This is the third stage: building bit table\n"
+             << endl;
+
+        cout << "This is the control bit table: " << endl;
+        for (int i = 0; i < snp_size; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout << "snp: " << i << "; genotype: " << j << " : ";
+                for (auto &c : control_bit_table[i][j])
+                {
+                    cout << c << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        cout << endl;
+
+        cout << "This is the case bit table: " << endl;
+        for (int i = 0; i < snp_size; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout << "snp: " << i << "; genotype: " << j << " : ";
+                for (auto &c : case_bit_table[i][j])
+                {
+                    cout << c << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        cout << "-------------------------------------------------------" << endl;
+        cout << endl;
+    }
+
     // initialize the contingency table
+    // (number of combinations) * (number of genotype combinations: 3 * 3 * 3)
     vector<vector<int>> control_contingency_table(combinations.size(), vector<int>(27, 0));
     vector<vector<int>> case_contingency_table(combinations.size(), vector<int>(27, 0));
 
@@ -172,8 +264,46 @@ int main(int argc, char *argv[])
     build_contingency_table(control_bit_table, control_contingency_table, combinations, control_size, snp_size);
     build_contingency_table(case_bit_table, case_contingency_table, combinations, case_size, snp_size);
 
-    // calculate the k2 score
+    if (debug)
+    {
+        cout << "This is the fourth stage: building contingency table\n"
+             << endl;
+
+        cout << "This is the control contingency table: " << endl;
+        for (int i = 0; i < combinations.size(); i++)
+        {
+            cout << "snp0: " << combinations[i][0] << "; snp1: " << combinations[i][1] << "; snp2: " << combinations[i][2] << endl;
+            for (int idx = 0; idx < 27; idx++)
+            {
+                int snp0_type = idx / 9;
+                int snp1_type = (idx % 9) / 3;
+                int snp2_type = idx % 3;
+                cout << "genotype: " << snp0_type << " " << snp1_type << " " << snp2_type << " : " << control_contingency_table[i][idx] << endl;
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+
+        cout << "This is the case contingency table: " << endl;
+        for (int i = 0; i < combinations.size(); i++)
+        {
+            cout << "snp0: " << combinations[i][0] << "; snp1: " << combinations[i][1] << "; snp2: " << combinations[i][2] << endl;
+            for (int idx = 0; idx < 27; idx++)
+            {
+                int snp0_type = idx / 9;
+                int snp1_type = (idx % 9) / 3;
+                int snp2_type = idx % 3;
+                cout << "genotype: " << snp0_type << " " << snp1_type << " " << snp2_type << " : " << case_contingency_table[i][idx] << endl;
+            }
+            cout << endl;
+        }
+    }
+
+    // calculate the k2 score and return the score and resulting combination
     pair<vector<char>, double> result = k2_score(control_contingency_table, case_contingency_table, snp_size, combinations);
+    cout << "The lowest K2 score: " << result.second << endl;
+    cout << "The most likely combination of snps: " << result.first[0] << " " << result.first[1] << " " << result.first[2] << endl;
 
     return 0;
 }
